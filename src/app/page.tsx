6ff135eb306da7fce7bb155b69f5ec3e54178e07
KITE <108491263+whoskite@users.noreply.github.com>
2025-03-13 19:11:19 -0700

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { DailyView } from "@/components/daily/daily-view";
+
+export default function Home() {
+  return (
+    <main className="container py-6">
+      <DailyView />
+    </main>
+  );
+}
